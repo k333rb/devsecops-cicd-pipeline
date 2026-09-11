1f42fbe59d637e8095667f2a6691f6e0340655ec
Khervee <kherveelagang24@gmail.com>
2026-09-11 16:50:03 +0800

    feat: add container apps environment and container app

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,3 +13,50 @@ resource "azurerm_container_registry" "main" {
   sku                 = "Basic"
   admin_enabled       = true
 }
+
+resource "azurerm_container_app_environment" "main" {
+  name                = "${var.project_name}-env"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+}
+
+resource "azurerm_container_app" "main" {
+  name                         = "${var.project_name}-app"
+  container_app_environment_id = azurerm_container_app_environment.main.id
+  resource_group_name          = azurerm_resource_group.main.name
+  revision_mode                = "Single"
+
+  # Scales to zero when idle, avoids paying for compute with no traffic
+  template {
+    min_replicas = 0
+    max_replicas = 1
+
+    container {
+      name   = "ticket-api"
+      image  = "${azurerm_container_registry.main.login_server}/ticket-pipeline-demo:latest"
+      cpu    = 0.25
+      memory = "0.5Gi"
+    }
+  }
+
+  # Lets the app pull private images from ACR using its admin credentials
+  registry {
+    server               = azurerm_container_registry.main.login_server
+    username              = azurerm_container_registry.main.admin_username
+    password_secret_name  = "acr-password"
+  }
+
+  secret {
+    name  = "acr-password"
+    value = azurerm_container_registry.main.admin_password
+  }
+
+  ingress {
+    external_enabled = true
+    target_port       = 5000
+    traffic_weight {
+      percentage      = 100
+      latest_revision = true
+    }
+  }
+}
